74067c9a67729975eb10db44cc5bc9e5a8288908
Girish Talekar <girish.talekar@capita.co.uk>
2019-01-08 15:34:05 +0530

    created initial setup

---
+++ pages/dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import css from './../App.scss';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div className={css.content}>
+                <p>Add</p>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
